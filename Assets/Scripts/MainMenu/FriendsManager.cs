dcb90c49cbdf802a61a24569e947dd41c5e7a7c1
astonished12 <dan_cehan@yahoo.com>
2017-09-12 11:12:36 +0300

    added file dialog

---
+++ Assets/Scripts/MainMenu/FriendsManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendsManager : MonoBehaviour {
+
+    //public GameObject friendOnlinePrefab;
+    //public GameObject friendOfflinePrefab;
+
+	void Start()
+    {
+        GameObject.Find("CanvasMenu").transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<Text>().text = "Welcome, " + UserData.userName;
+    }
+}
--- Assets/Scripts/MainMenu/MainButtonsManager.cs
+++ Assets/Scripts/MainMenu/MainButtonsManager.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class MainButtonsManager : MonoBehaviour {
 
     public GameObject profilePanel;
 
-	public void ProfileButtonPressed()
+    public void ProfileButtonPressed()
     {
         GameObject profilePanelTmp = Instantiate(profilePanel);
         profilePanelTmp.transform.parent = GameObject.Find("CanvasMenu").transform;
         profilePanelTmp.gameObject.transform.position = GameObject.Find("CanvasMenu").transform.position;
-        
+
     }
 
-    
+
 }
