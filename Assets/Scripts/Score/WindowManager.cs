f4c20e262d510db29e47749b406b0c78e9a18545
astonished12 <dan_cehan@yahoo.com>
2017-12-05 14:45:18 +0200

    working on score board

---
+++ Assets/Scripts/Score/WindowManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowManager : MonoBehaviour {
+
+    public GameObject scoreBoard;
+    
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.Tab)) {
+            scoreBoard.SetActive( !scoreBoard.activeSelf );
+        }
+    }
+}
